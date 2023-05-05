d795ac1e78a5d64e56fdf135fcad52289f7f5d39
qq358856605 <80167086+qq358856605@users.noreply.github.com>
2023-05-05 00:55:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 // Require http module
 
+const http = require('http');
+
 // Require fs module
+
+const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
 
+const minimist = require('minimist');
+
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
+
+const arg = minimist(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
+
+const port = arg['port'] || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
@@ -17,9 +27,16 @@
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
+var temp;
 
-
-
+fs.readFile('./public/index.html', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    } 
+    
+    temp = data;
+});
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -28,14 +45,19 @@
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-
+const server = http.createServer(function(req, res) {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+    res.end(temp)
+});
 
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-
+server.listen(port);
+console.log(`Server listening on port ${port}`);
 
 
 // That's it! You're all done!
